568bcb0f97cb4893eb7cc4de9e95fddc308f016e
Kim, Hyojin <dosoh77@gmail.com>
2020-05-21 00:07:00 +0900

    #개선코드 anonymouse inner class

--- app/src/main/java/net/skhu/e04firebase/Firebase1Activity.java
+++ app/src/main/java/net/skhu/e04firebase/Firebase1Activity.java
@@ -15,35 +15,29 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Firebase1Activity extends AppCompatActivity {
+public class Firebase1Activity extends AppCompatActivity implements ValueEventListener {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase1);
+        TextView textView = (TextView)findViewById(R.id.textView);
         final DatabaseReference myData01 = FirebaseDatabase.getInstance().getReference("myData01");
-        ValueEventListener listener1 = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                TextView textView = (TextView)findViewById(R.id.textView);
-                textView.setText(value);
-                Log.d("내태그", "받은 데이터: " + value);
-            }
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.e("내태그", "서버 에러: ", error.toException());
-            }
-        };
-        myData01.addValueEventListener(listener1);
+        myData01.addValueEventListener(this);
         Button button = (Button)findViewById(R.id.btnSaveIntoServer);
-        View.OnClickListener listener2 = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText editText = (EditText)findViewById(R.id.editText);
-                String s = editText.getText().toString();
-                myData01.setValue(s);
-            }
-        };
-        button.setOnClickListener(listener2);
+        final EditText editText = (EditText)findViewById(R.id.editText);
+        button.setOnClickListener( (view) -> myData01.setValue( editText.getText().toString() ) );
+    }
+    @Override
+    // ValueEventListener의 메소드 재정의
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        String value = dataSnapshot.getValue(String.class);
+        textView.setText(value);
+        Log.d("내태그", "받은 데이터: " + value);
+    }
+    @Override
+    // ValueEventListener의 메소드 재정의
+    public void onCancelled(DatabaseError error) {
+        Log.e("내태그", "서버 에러: ", error.toException());
     }
 }

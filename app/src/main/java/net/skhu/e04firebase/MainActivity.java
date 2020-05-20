3a5a3d77b1edf6e47ccaeeec24c4bbee971dd382
Kim, Hyojin <dosoh77@gmail.com>
2020-05-21 00:01:51 +0900

    Initial commit

---
+++ app/src/main/java/net/skhu/e04firebase/MainActivity.java
@@ -0,0 +1,14 @@
+package net.skhu.e04firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
